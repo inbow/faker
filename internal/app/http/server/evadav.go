49d7315c1eb50972023698e996526cf7056af215
Konstantin <bkbokov@wisebits.com>
2020-07-15 18:13:20 +0300

    Added push response for evadav

--- internal/app/http/server/evadav.go
+++ internal/app/http/server/evadav.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"github.com/google/uuid"
+	"github.com/tada-team/kozma"
 	"net/http"
 
 	jsoniter "github.com/json-iterator/go"
@@ -25,6 +27,33 @@ func (s *Server) EvadavPopunder(ctx *atreugo.RequestCtx) error {
 	}
 
 	ctx.Response.Header.Set("Content-Type", "application/javascript")
+	ctx.SetStatusCode(http.StatusOK)
+	ctx.SetBody(data)
+
+	return nil
+}
+
+func (s *Server) EvadavPush(ctx *atreugo.RequestCtx) error {
+	handlerResponse := &atom.EvadavPushResponse{
+
+		Id:    uuid.New().String(),
+		Title: kozma.Say(),
+		Descr: kozma.Say(),
+		Icon:  "https://push.example.com/icon/dc3e7a05-e267-4a7a-88be-cec6e7asdf9f",
+		Image: "https://push.example.com/image/dc3e7a05-e267-4a7a-88be-cec6e7asdf9f",
+		Link:  s.generator.URLOrDefault(ctx.UserValue(string(URL)).(string)),
+		Cpc:   s.generator.PriceOrDefault(ctx.UserValue(string(Price)).(float64), generator.CPV),
+	}
+
+	data, err := jsoniter.Marshal(handlerResponse)
+	if err != nil {
+		ctx.SetStatusCode(http.StatusBadGateway)
+		ctx.SetBody([]byte(err.Error()))
+
+		return nil
+	}
+
+	ctx.Response.Header.Set("Content-Type", "application/json")
 	ctx.SetStatusCode(http.StatusOK)
 	ctx.SetBody(data)
 
